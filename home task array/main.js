1138ff777d3d379a497f2d8af4a23620bc3850b5
Safaroff90 <110519574+Safaroff90@users.noreply.github.com>
2022-10-27 22:32:56 +0400

    added
    newadd

---
+++ home task array/main.js
@@ -0,0 +1,24 @@
+let arr1 = [1, 2, 3, 4];
+let arr2 = [1, 2, 3, 4];
+
+// let arr3 = arr1 === arr2;
+// console.log(arr3);
+
+// let same =
+//   arr1.length == arr2.length &&
+//   arr1.every(function (element, index) {
+//     return element === arr2[index];
+//   });
+
+// console.log(JSON.stringify(arr1) === JSON.stringify(arr2));
+
+// console.log(arr1.toString() === arr2.toString());
+
+// for (let i = 0; i < arr1.length; i++) {
+//   for (let j = 0; j < arr2.length; j++) {
+//     if (arr1[i] === arr2[j]) {
+//       return true;
+//     }
+//     return false;
+//   }
+// }
